e72ac2bb748a97c269b2093ca61349771a18defb
Ubuntu <ubuntu@ip-172-31-84-227.ec2.internal>
2020-11-22 19:52:53 +0000

    proper code style

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2020 Ryan Walsh all right reserved
+//
+// Created by Ryan Walsh
+// Created on November 2020
+// This is a "Hello World" program with proper style
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+}
